0d72471a3070b28f3320545e159be774edb9d03b
bt2fed <bt2@students.federation.edu.au>
2024-04-24 18:52:05 +1000

    Added settings and custom page to home screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Brightness _currentBrightness = Brightness.light;
+
+  void toggleTheme() {
+    setState(() {
+      _currentBrightness =
+          (_currentBrightness == Brightness.light) ? Brightness.dark : Brightness.light;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      theme: ThemeData(brightness: _currentBrightness),
+      home: HomeScreen(toggleTheme: toggleTheme),
       routes: {
-        '/create': (context) => CreatePage(), // Define route for CreatePage
-      }
+        '/create': (context) => CreatePage(),
+      },
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
+  final VoidCallback toggleTheme;
+
+  HomeScreen({required this.toggleTheme});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue, // Set the background color to blue
+      backgroundColor: Colors.blue,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Hello! Welcome to the Body Check App', // Text to display
+              'Hello! Welcome to the Body Check App',
               style: TextStyle(
                 fontSize: 40,
-                color: Colors.white, // Text color
+                color: Colors.white,
                 fontWeight: FontWeight.bold,
               ),
             ),
-
             ElevatedButton(
               onPressed: () {
-                // Navigate to the CreatePage when the "Creation Page" button is pressed
                 Navigator.pushNamed(context, '/create');
               },
-              child: Text('Creation Page'), // Button text
+              child: Text('Creation Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage(toggleTheme: toggleTheme)));
+              },
+              child: Text('Settings'),
             ),
           ],
         ),
@@ -46,19 +71,195 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class CreatePage extends StatelessWidget {
+class CreatePage extends StatefulWidget {
+  @override
+  _CreatePageState createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  TextEditingController _bodyCheckController = TextEditingController();
+  double _rating = 0;
+  bool _isCompleted = false;
+  String _selectedBodyPart = 'Head';
+  bool _isCustomBodyPartSelected = false;
+  String _customBodyPart = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Set the background color to white
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Create Custom Body Check'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Select Body Part:',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 'Head',
+                  groupValue: _selectedBodyPart,
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedBodyPart = value!;
+                      _isCustomBodyPartSelected = false;
+                    });
+                  },
+                ),
+                Text('Head'),
+                Radio(
+                  value: 'Torso',
+                  groupValue: _selectedBodyPart,
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedBodyPart = value!;
+                      _isCustomBodyPartSelected = false;
+                    });
+                  },
+                ),
+                Text('Torso'),
+                Radio(
+                  value: 'Legs',
+                  groupValue: _selectedBodyPart,
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedBodyPart = value!;
+                      _isCustomBodyPartSelected = false;
+                    });
+                  },
+                ),
+                Text('Legs'),
+                Radio(
+                  value: 'Custom',
+                  groupValue: _selectedBodyPart,
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedBodyPart = value!;
+                      _isCustomBodyPartSelected = true;
+                    });
+                  },
+                ),
+                Text('Custom'),
+              ],
+            ),
+            SizedBox(height: 20),
+            if (_isCustomBodyPartSelected)
+              TextField(
+                onChanged: (value) {
+                  setState(() {
+                    _customBodyPart = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Custom Body Part',
+                ),
+              ),
+            SizedBox(height: 20),
+            Text(
+              'Rate Condition:',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Slider(
+              value: _rating,
+              onChanged: (value) {
+                setState(() {
+                  _rating = value;
+                });
+              },
+              min: 0,
+              max: 10,
+              divisions: 10,
+              label: _rating.toStringAsFixed(1),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Checkbox(
+                  value: _isCompleted,
+                  onChanged: (value) {
+                    setState(() {
+                      _isCompleted = value!;
+                    });
+                  },
+                ),
+                Text(
+                  'Mark as Completed',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                onPressed: () {
+                  String bodyCheck = _bodyCheckController.text;
+                  print('Body Part: $_selectedBodyPart');
+                  print('Condition Rating: $_rating');
+                  print('Is Completed: $_isCompleted');
+                  if (_isCustomBodyPartSelected) {
+                    print('Custom Body Part: $_customBodyPart');
+                  }
+                  print('Custom Body Check: $bodyCheck');
+                  _bodyCheckController.clear();
+                },
+                child: Text('Save Body Check'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _bodyCheckController.dispose();
+    super.dispose();
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  final VoidCallback toggleTheme;
+
+  SettingsPage({required this.toggleTheme});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
       body: Center(
-        child: Text(
-          'Hello there, welcome to the create page for custom body checks', // Text to display
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.black, // Text color
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Switch(
+              value: Theme.of(context).brightness == Brightness.dark,
+              onChanged: (value) {
+                toggleTheme();
+              },
+            ),
+          ],
         ),
       ),
     );
