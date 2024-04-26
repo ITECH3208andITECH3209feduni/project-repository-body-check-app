f035aa4b51a42a75d8036bbf17e85524930f5a3d
bt2fed <bt2@students.federation.edu.au>
2024-04-26 11:40:37 +1000

    Added scheduling body check functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); // Change this to MyApp
 }
 
 class MyApp extends StatefulWidget {
@@ -26,6 +26,7 @@ class _MyAppState extends State<MyApp> {
       home: HomeScreen(toggleTheme: toggleTheme),
       routes: {
         '/create': (context) => CreatePage(),
+        '/schedule': (context) => SchedulePage(),
       },
     );
   }
@@ -60,7 +61,18 @@ class HomeScreen extends StatelessWidget {
             ),
             ElevatedButton(
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage(toggleTheme: toggleTheme)));
+                Navigator.pushNamed(context, '/schedule');
+              },
+              child: Text('Schedule'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SettingsPage(toggleTheme: toggleTheme), // Pass toggleTheme here
+                  ),
+                );
               },
               child: Text('Settings'),
             ),
@@ -70,6 +82,7 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
 
 class CreatePage extends StatefulWidget {
   @override
@@ -232,32 +245,88 @@ class _CreatePageState extends State<CreatePage> {
   }
 }
 
-class SettingsPage extends StatelessWidget {
-  final VoidCallback toggleTheme;
 
-  SettingsPage({required this.toggleTheme});
+class SettingsPage extends StatefulWidget {
+  final VoidCallback toggleTheme; 
+
+  SettingsPage({required this.toggleTheme}); 
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+
+
+class _SettingsPageState extends State<SettingsPage> {
+  Brightness _currentBrightness = Brightness.light;
+
+   void toggleTheme() {
+    setState(() {
+      _currentBrightness =
+          (_currentBrightness == Brightness.light) ? Brightness.dark : Brightness.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+    ThemeData _currentTheme = ThemeData(brightness: _currentBrightness);
+
+    return MaterialApp(
+      theme: _currentTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Settings'),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context); // Navigate back to the previous screen
+            },
+          ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Switch(
-              value: Theme.of(context).brightness == Brightness.dark,
-              onChanged: (value) {
-                toggleTheme();
-              },
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                _currentBrightness == Brightness.dark ? 'Dark Mode' : 'Light Mode',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ListTile(title: Text('Option 1')),
+                        ListTile(title: Text('Option 2')),
+                        ListTile(title: Text('Option 3')),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        _currentBrightness == Brightness.dark ? 'Light Mode' : 'Dark Mode',
+                        style: TextStyle(fontSize: 12, fontWeight: FontWeight.normal),
+                      ),
+                      Switch(
+                        value: _currentBrightness == Brightness.dark,
+                        onChanged: (value) {
+                          toggleTheme();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
@@ -265,3 +334,98 @@ class SettingsPage extends StatelessWidget {
     );
   }
 }
+
+class SchedulePage extends StatefulWidget {
+  @override
+  _SchedulePageState createState() => _SchedulePageState();
+}
+
+class _SchedulePageState extends State<SchedulePage> {
+  int _selectedOption = 1;
+  TimeOfDay _selectedTime = TimeOfDay.now();
+
+  void _selectTime(BuildContext context) async {
+    final TimeOfDay? newTime = await showTimePicker(
+      context: context,
+      initialTime: _selectedTime,
+    );
+
+    if (newTime != null) {
+      setState(() {
+        _selectedTime = newTime;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Schedule Body Checks'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Select Option:',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            DropdownButton<int>(
+              value: _selectedOption,
+              onChanged: (int? newValue) {
+                setState(() {
+                  _selectedOption = newValue!;
+                });
+              },
+              items: <int>[1, 2, 3, 4].map((int value) {
+                return DropdownMenuItem<int>(
+                  value: value,
+                  child: Text('Option $value'),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Select Time:',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            InkWell(
+              onTap: () {
+                _selectTime(context);
+              },
+              child: Row(
+                children: [
+                  Icon(Icons.access_time),
+                  SizedBox(width: 10),
+                  Text(
+                    _selectedTime.format(context),
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Implement your scheduling logic here
+                print('Selected Option: $_selectedOption');
+                print('Selected Time: ${_selectedTime.format(context)}');
+              },
+              child: Text('Confirm'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
