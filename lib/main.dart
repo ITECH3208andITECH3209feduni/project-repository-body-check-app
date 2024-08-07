7001496dd8d379d794b45bd6b02a755f19986512
CoreyGarth <166610514+CoreyGarth@users.noreply.github.com>
2024-08-07 20:58:23 +1000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,20 @@ import 'package:provider/provider.dart';
 void main() {
   runApp(
     ChangeNotifierProvider(
-      create: (context) => ThemeManager(ThemeData(brightness: Brightness.light)),
+      create: (context) => AppStateManager(ThemeData(brightness: Brightness.light)),
       child: MyApp(),
     ),
   );
 }
 
-class ThemeManager with ChangeNotifier {
+class AppStateManager with ChangeNotifier {
   ThemeData _themeData;
+  String _selectedMascot = 'assets/mascot_shark.png';
 
-  ThemeManager(this._themeData);
+  AppStateManager(this._themeData);
 
   ThemeData get themeData => _themeData;
+  String get selectedMascot => _selectedMascot;
 
   void toggleTheme() {
     _themeData = _themeData.brightness == Brightness.light
@@ -23,14 +25,19 @@ class ThemeManager with ChangeNotifier {
         : ThemeData(brightness: Brightness.light);
     notifyListeners();
   }
+
+  void setMascot(String mascot) {
+    _selectedMascot = mascot;
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final themeManager = Provider.of<ThemeManager>(context);
+    final appStateManager = Provider.of<AppStateManager>(context);
     return MaterialApp(
-      theme: themeManager.themeData,
+      theme: appStateManager.themeData,
       home: HomeScreen(),
       routes: {
         '/create': (context) => CreatePage(),
@@ -44,6 +51,7 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final appStateManager = Provider.of<AppStateManager>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
@@ -58,7 +66,14 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Image.asset(
+              appStateManager.selectedMascot,
+              width: 150,
+              height: 150,
+            ),
+            SizedBox(height: 20),
             Text('Welcome to the Body Check App'),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: () => Navigator.pushNamed(context, '/create'),
               child: Text('Create Body Check'),
@@ -207,131 +222,130 @@ class _CreatePageState extends State<CreatePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Create Custom Body Check'),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Select Body Part:'),
-              Row(
-                children: [
-                  Radio(
-                    value: 'Head',
-                    groupValue: _selectedBodyPart,
-                    onChanged: (String? value) {
-                      setState(() {
-                        _selectedBodyPart = value!;
-                        _isCustomBodyPartSelected = false;
-                      });
-                    },
-                  ),
-                  Text('Head'),
-                  Radio(
-                    value: 'Torso',
-                    groupValue: _selectedBodyPart,
-                    onChanged: (String? value) {
-                      setState(() {
-                        _selectedBodyPart = value!;
-                        _isCustomBodyPartSelected = false;
-                      });
-                    },
-                  ),
-                  Text('Torso'),
-                  Radio(
-                    value: 'Legs',
-                    groupValue: _selectedBodyPart,
-                    onChanged: (String? value) {
-                      setState(() {
-                        _selectedBodyPart = value!;
-                        _isCustomBodyPartSelected = false;
-                      });
-                    },
-                  ),
-                  Text('Legs'),
-                  Radio(
-                    value: 'Custom',
-                    groupValue: _selectedBodyPart,
-                    onChanged: (String? value) {
-                      setState(() {
-                        _selectedBodyPart = value!;
-                        _isCustomBodyPartSelected = true;
-                      });
-                    },
-                  ),
-                  Text('Custom'),
-                ],
-              ),
-              if (_isCustomBodyPartSelected)
-                TextFormField(
-                  onChanged: (value) {
-                    setState(() {
-                      _customBodyPart = value;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Custom Body Part',
-                  ),
-                  validator: (value) {
-                    if (_isCustomBodyPartSelected && value!.isEmpty) {
-                      return 'Please enter a custom body part';
-                    }
-                    return null;
-                  },
-                ),
-              SizedBox(height: 20),
-              Text(
-                'Rate Condition:',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Slider(
-                value: _rating,
-                onChanged: (value) {
-                  setState(() {
-                    _rating = value;
-                  });
-                },
-                min: 0,
-                max: 10,
-                divisions: 10,
-                label: _rating.toStringAsFixed(1),
-              ),
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  Checkbox(
-                    value: _isCompleted,
-                    onChanged: (value) {
-                      setState(() {
-                        _isCompleted = value!;
-                      });
-                    },
-                  ),
-                  Text(
-                    'Mark as Completed',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _validateAndSubmit,
-                child: Text('Save Body Check'),
-              ),
-            ],
+        appBar: AppBar(
+          title: Text('Create Custom Body Check'),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
         ),
+        body: Padding(
+        padding: const EdgeInsets.all(20.0),
+    child: Form(
+    key: _formKey,
+    child: ListView(
+    children: [
+    Text('Select Body Part:'),
+    Row(
+    children: [
+    Radio<String>(
+    value: 'Head',
+    groupValue: _selectedBodyPart,
+    onChanged: (String? value) {
+    setState(() {
+    _selectedBodyPart = value!;
+    _isCustomBodyPartSelected = false;
+    });
+    },
+    ),
+    Text('Head'),
+    Radio<String>(
+    value: 'Torso',
+    groupValue: _selectedBodyPart,
+    onChanged: (String? value) {
+    setState(() {
+    _selectedBodyPart = value!;
+    _isCustomBodyPartSelected = false;
+    });
+    },
+    ),
+    Text('Torso'),
+    Radio<String>(
+    value: 'Legs',
+    groupValue: _selectedBodyPart,
+    onChanged: (String? value) {
+    setState(() {
+    _selectedBodyPart = value!;
+    _isCustomBodyPartSelected = false;
+    });
+    },
+    ),
+    Text('Legs'),
+    Radio<String>(
+    value: 'Custom',
+    groupValue: _selectedBodyPart,
+    onChanged: (String? value) {
+    setState(() {
+    _selectedBodyPart = value!;
+    _isCustomBodyPartSelected = true;
+    });
+    },
+    ),
+    Text('Custom'),
+    ],
+    ),
+      if (_isCustomBodyPartSelected)
+        TextFormField(
+          onChanged: (value) {
+            setState(() {
+              _customBodyPart = value;
+            });
+          },
+          decoration: InputDecoration(
+            labelText: 'Custom Body Part',
+          ),
+          validator: (value) {
+            if (_isCustomBodyPartSelected && value!.isEmpty) {
+              return 'Please enter a custom body part';
+            }
+            return null;
+          },
+        ),
+      SizedBox(height: 20),
+      Text(
+        'Rate Condition:',
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
       ),
+      Slider(
+        value: _rating,
+        onChanged: (value) {
+          setState(() {
+            _rating = value;
+          });
+        },
+        min: 0,
+        max: 10,
+        divisions: 10,
+        label: _rating.toStringAsFixed(1),
+      ),
+      SizedBox(height: 20),
+      Row(
+        children: [
+          Checkbox(
+            value: _isCompleted,
+            onChanged: (value) {
+              setState(() {
+                _isCompleted = value!;
+              });
+            },
+          ),
+          Text(
+            'Mark as Completed',
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+      SizedBox(height: 20),
+      ElevatedButton(
+        onPressed: _validateAndSubmit,
+        child: Text('Save Body Check'),
+      ),
+    ],
+    ),
+    ),
+        ),
       floatingActionButton: FloatingActionButton(
         onPressed: _resetForm,
         tooltip: 'Undo',
@@ -341,7 +355,9 @@ class _CreatePageState extends State<CreatePage> {
   }
 }
 
-class SchedulePage extends StatefulWidget {
+
+
+    class SchedulePage extends StatefulWidget {
   @override
   _SchedulePageState createState() => _SchedulePageState();
 }
@@ -452,9 +468,15 @@ class _SchedulePageState extends State<SchedulePage> {
 }
 
 class SettingsPage extends StatelessWidget {
+  final List<String> mascots = [
+    'assets/mascot_fox.png',
+    'assets/mascot_shark.png',
+    'assets/mascot_lizard.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    final themeManager = Provider.of<ThemeManager>(context);
+    final appStateManager = Provider.of<AppStateManager>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
@@ -463,14 +485,63 @@ class SettingsPage extends StatelessWidget {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: ListTile(
-        title: Text('Toggle Theme'),
-        trailing: Switch(
-          value: themeManager.themeData.brightness == Brightness.dark,
-          onChanged: (value) {
-            themeManager.toggleTheme();
-          },
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Select Mascot:',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.all(16.0),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 16.0,
+                mainAxisSpacing: 16.0,
+              ),
+              itemCount: mascots.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    appStateManager.setMascot(mascots[index]);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Selected ${mascots[index].split('/').last}')),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: appStateManager.selectedMascot == mascots[index]
+                            ? Theme.of(context).colorScheme.secondary
+                            : Theme.of(context).primaryColor,
+                        width: appStateManager.selectedMascot == mascots[index] ? 3 : 1,
+                      ),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Image.asset(
+                      mascots[index],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Dark Mode'),
+            trailing: Switch(
+              value: appStateManager.themeData.brightness == Brightness.dark,
+              onChanged: (value) {
+                appStateManager.toggleTheme();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
