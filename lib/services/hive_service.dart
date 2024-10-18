575d67b4885b060bc5453690c9cc3ff36a913ce9
CoreyGarth <166610514+CoreyGarth@users.noreply.github.com>
2024-10-18 17:15:37 +1000

    Update hive_service.dart

--- lib/services/hive_service.dart
+++ lib/services/hive_service.dart
@@ -1,1 +1,24 @@
-// TODO Implement this library.
+import 'package:hive/hive.dart';
+import 'package:itech3209/models/symptom_record.dart';
+
+class HiveService {
+  static const String boxName = 'symptomRecords';
+
+  Future<void> addSymptomRecord(SymptomRecord record) async {
+    final box = await Hive.openBox<SymptomRecord>(boxName);
+    await box.add(record);
+  }
+
+  Future<List<SymptomRecord>> getSymptomRecords() async {
+    final box = await Hive.openBox<SymptomRecord>(boxName);
+    return box.values.toList();
+  }
+
+  Future<void> updateSymptomRecord(SymptomRecord record) async {
+    await record.save();
+  }
+
+  Future<void> deleteSymptomRecord(SymptomRecord record) async {
+    await record.delete();
+  }
+}
